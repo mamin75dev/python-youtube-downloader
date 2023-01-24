2cdb4c12bb410304df6ef0b217d4787afbb69542
Mohamadamin Shariatmadar <shariatmadar@hasingroup.com>
2023-01-24 15:00:49 +0330

    bug fix

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 import pytube
 from tqdm import tqdm
 
-def percent(tem, total):
-    perc = float(100) - (float(tem) / float(total)) * float(100)
-    return perc
-    
-def progress_function(stream, chunk, bytes_remaining):
-    # print(bytes_remaining)
-    size = stream.filesize
-    # print(str(percent(bytes_remaining, size))+'%')
-    # tqdm(percent(bytes_remaining, size))
-    per = int(percent(bytes_remaining, size))
-    t.update(size - bytes_remaining)
+# global prev_remaining
+# prev_remaining = 0.0
+class Downloader:
+    def __init__(self, path):
+        self.file = pytube.YouTube(path, on_progress_callback=self.progress_function).streams.get_highest_resolution()
+        self.t = tqdm(total=self.file.filesize,  unit='B', unit_scale=True, unit_divisor=1024, miniters=1)
+        self.prev_remaining = self.file.filesize
 
-file = pytube.YouTube(input("Enter your link"), on_progress_callback=progress_function).streams.get_highest_resolution()
-t = tqdm(total=file.filesize,  unit='B', unit_scale=True, unit_divisor=1024, miniters=1)
-file.download("./")
+    def percent(self, tem, total):
+        perc = float(100) - (float(tem) / float(total)) * float(100)
+        return perc
+        
+    def progress_function(self, stream, chunk, bytes_remaining):
+        # print(bytes_remaining)
+        size = stream.filesize
+        # print(str(percent(bytes_remaining, size))+'%')
+        # tqdm(percent(bytes_remaining, size))
+        # t.update(prev_remaining - float(bytes_remaining))
+        self.t.update(self.prev_remaining - float(bytes_remaining))
+        self.prev_remaining = float(bytes_remaining)
+
+
+    def download_file(self):
+        self.file.download("./")
+        self.t.close()
+    
+
+downloader = Downloader("https://youtu.be/GVlSTgnUXfg")
+downloader.download_file()
